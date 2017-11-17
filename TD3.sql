a469bd03a2aefd18ce0af1791ce522f43bced350
alexConts <alexandrecontassot@free.fr>
2017-11-17 12:30:13 +0100

    Update TD3.sql

--- TD3.sql
+++ TD3.sql
@@ -6,3 +6,8 @@ select * from pointdevente;
 # Question 2
 select nom, ville, anneeOuverture from pointdevente;
 # Question 3
+select nom from pointdevente where anneeOuverture < 1992;
+# Question 4
+select distinct labelJ_M from commande; #distinct sert à ne pas afficher les doublons
+# Question 5
+select distinct labelJ_M from commande order by labelJ_M ASC;
